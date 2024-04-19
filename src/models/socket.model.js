8117088653ba670daa161730026a7158f0a3570c
alexba2004 <j.alejandro.briones.a22@gmail.com>
2024-04-19 17:25:20 -0600

    update

--- src/models/socket.model.js
+++ src/models/socket.model.js
@@ -5,7 +5,7 @@ const socketSchema = new Schema(
     led: Number,
     potentiometer: Number,
     distance_sensor: Number,
-    costtemperature_sensor: Number,
+    temperature_sensor: Number,
   },
   {
     versionKey: false,
